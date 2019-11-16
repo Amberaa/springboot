a2aee4ddacbf673a9bb38cf26aa0c6e0d3905949
liuxiaoying <liuxiaoying@bonc.com.cn>
2019-11-16 16:12:10 +0800

    .

--- springbootmybatis/src/main/java/com/example/springbootmybatis/SpringbootmybatisApplication.java
+++ springbootmybatis/src/main/java/com/example/springbootmybatis/SpringbootmybatisApplication.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @PropertySource(value = "classpath:application.properties",encoding = "UTF-8")
-@MapperScan("com.example.springbootmybatis.dao")/*扫描dao，创建对象*/
+@MapperScan("com.example.springbootmybatis.dao") /*扫描dao，创建对象*/
 public class SpringbootmybatisApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootmybatisApplication.class, args);
